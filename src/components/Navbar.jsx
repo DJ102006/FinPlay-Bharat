ee60d1b46b0ca091a6779b01af08bec6c0de3bfd
Dhairya Jain <jaindhairya2710@gmail.com>
2026-02-25 12:09:28 +0530

    Navbar structure complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,26 @@
 import Logo from "./Logo"
+import "./Navbar.css"
 
 export default function Navbar() {
     return (
-        <div>
+        <div className="navbar">    
+
             <Logo />
+
+            <div className="nav-center">
+                <a href="#">Home</a>
+                <a href="#">Features</a>
+                <a href="#">Demo</a>
+                <a href="#">How it Works</a>
+                <a href="#">Company</a>
+            </div>
+
+            <div className="nav-right">
+                <button>Login</button>
+                <button>Toggle</button>
+            </div>
+            
+            
         </div>
     )
 }
